ae5ae6a8555ce6cb6a8ee7fe048b43f1d6e2fede
thalisonh <thalison_henrique@hotmail.com>
2021-03-23 22:17:06 -0300

    criando pagina de formulario

---
+++ src/main/java/br/com/todolist/controller/TodoController.java
@@ -0,0 +1,15 @@
+package br.com.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("todo")
+public class TodoController {
+    
+    @GetMapping("formulario")
+    public String formulario(){
+        return "todo/formulario";
+    }
+}
